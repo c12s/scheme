da4a5ddbe7e1ab8386859f1fae73769501f873f9
milossimic <milossimicsimo@gmail.com>
2020-02-12 00:28:26 +0100

    meridian files

--- gen.sh
+++ gen.sh
@@ -5,4 +5,5 @@ protoc -I. --go_out=plugins=grpc:$GOPATH/src/. celestial/celestial.proto
 protoc -I. --go_out=plugins=grpc:$GOPATH/src/. gravity/gravity.proto
 protoc -I. --go_out=plugins=grpc:$GOPATH/src/. stellar/stellar.proto
 protoc -I. --go_out=plugins=grpc:$GOPATH/src/. flusher/flusher.proto
+protoc -I. --go_out=plugins=grpc:$GOPATH/src/. meridian/meridian.proto
 echo "Generating done."
